efba67a730da943905f26974a8ea9f177bddf6d1
infurno <hal.borland@gmail.com>
2020-12-22 12:18:51 -0600

    updated site

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
-    <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
+    <h1 class="title">Prototype for Buffalo River Canoes</h1>
     <h2 class="subtitle">Boilerplate</h2>
+    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fb2SmaOVOx9LxYN42xhtUoG%2FBuffalo-River-Canoes-Project%3Fnode-id%3D0%253A1" allowfullscreen></iframe>
+
   </div>
 </template>
 
 <script>
-// import Logo from '~/components/Logo.vue'
+    // import Logo from '~/components/Logo.vue'
 
 export default {
   // components: {
